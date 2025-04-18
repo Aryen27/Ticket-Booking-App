257635d4022b6e603b41a28a18565baa805fe9fc
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-19 02:44:41 +0530

    Added services and a dummy DB

---
+++ app/src/main/java/ticket/booking/services/TrainService.java
@@ -0,0 +1,11 @@
+package ticket.booking.services;
+import ticket.booking.entities.User;
+
+public class TrainService {
+    private User user;
+
+    public UserBookingService(User user1){
+        this.user= user1;
+
+    }
+}
