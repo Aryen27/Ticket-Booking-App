4700648680b5e562ec9bb0479529b5d0cd9a20d3
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-21 23:55:28 +0530

    Added object mapper to deserialize json data and store the list of users

--- app/src/main/java/ticket/booking/services/TrainService.java
+++ app/src/main/java/ticket/booking/services/TrainService.java
@@ -4,8 +4,5 @@ import ticket.booking.entities.User;
 public class TrainService {
     private User user;
 
-    public UserBookingService(User user1){
-        this.user= user1;
 
-    }
 }
--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,4 +1,25 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class UserBookingService {
+    private User user;
+    private List<User> userList;
+
+    private static final String USERS_PATH= "../localDb/Users.json";
+    private static final ObjectMapper objectMapper= new ObjectMapper();
+
+
+    public UserBookingService(User user1) throws IOException
+    {
+        this.user= user1;
+        File users= new File(USERS_PATH);
+        userList= objectMapper.readValue(users, new TypeReference<List<User>>() {}); //TypeRef is to deserialize json data to list of users(List<User>)
+    }
 }
